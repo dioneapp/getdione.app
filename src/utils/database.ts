0fad3562e34952945b1a486155d6a24ea32e907c
bygimenez <davidalbal123@gmail.com>
2025-01-20 16:44:14 +0100

    refactor: :sparkles: better supabase handler

---
+++ src/utils/database.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY as string;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Please provide PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_KEY in your .env file');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
