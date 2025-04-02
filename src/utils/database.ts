7a16a2392bad534bdfdfa1317792322e4df8aa63
deeivihh <davidalbal123@gmail.com>
2025-04-02 04:41:51 +0200

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -11,6 +11,7 @@ if (!supabaseUrl || !supabaseKey) {
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
+    autoRefreshToken: true,
     persistSession: true,
   },
 });
