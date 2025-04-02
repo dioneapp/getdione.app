f09c17abccf2197ec5faa056f8c7a6b5cbb29e53
deeivihh <davidalbal123@gmail.com>
2025-04-02 04:28:21 +0200

    Update database.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,14 +4,13 @@ const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string;
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
-	throw new Error(
-		"Please provide PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_KEY in your .env file",
-	);
+  throw new Error(
+    "Please provide PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_KEY in your .env file",
+  );
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey 				    	
-	{
-		auth: {
-			autoRefreshToken: true,
-	},
-},);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRefreshToken: true,
+  },
+});
