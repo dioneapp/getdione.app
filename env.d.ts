a6631fed4f1c412fdb5c0b418a34cfa69d563352
deeivihh <deeivihh@hotmail.com>
2025-05-10 18:23:04 +0200

    Fix changelog

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,7 @@ declare namespace App {
         BETA_DISCORD_WEBHOOK_URL: string;
         PUBLIC_SUPABASE_URL: string;
         PUBLIC_SUPABASE_KEY: string;
+        GITHUB_TOKEN: string;
       };
     };
   }
