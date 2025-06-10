b8340e6c4c088245476a00d30db740410218a598
blaisewf <blaise@applio.org>
2025-06-10 15:52:34 +0200

    bad idea

--- env.d.ts
+++ env.d.ts
@@ -5,7 +5,7 @@ declare namespace App {
         FEATURED_DISCORD_WEBHOOK_URL: string;
         BETA_DISCORD_WEBHOOK_URL: string;
         PUBLIC_SUPABASE_URL: string;
-        PRIVATE_SUPABASE_KEY: string;
+        PUBLIC_SUPABASE_KEY: string;
         GITHUB_TOKEN: string;
       };
     };
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.PRIVATE_SUPABASE_KEY as string;
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error(
-    "Please provide PUBLIC_SUPABASE_URL and PRIVATE_SUPABASE_KEY in your .env file",
+    "Please provide PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_KEY in your .env file",
   );
 }
 
