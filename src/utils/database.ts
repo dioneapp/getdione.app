79896b8aff1687ca335036a4f02cad64c665958b
deeivihh <deeivihh@hotmail.com>
2025-06-11 03:32:23 +0200

    Make supabase keys private

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY as string;
+const supabaseUrl = import.meta.env.SUPABASE_URL as string;
+const supabaseKey = import.meta.env.SUPABASE_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
 	throw new Error(
-		"Please provide PUBLIC_SUPABASE_URL and PUBLIC_SUPABASE_KEY in your .env file",
+		"Please provide SUPABASE_URL and SUPABASE_KEY in your .env file",
 	);
 }
 
