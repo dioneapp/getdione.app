918dc19f69bd73f6ba4cb38ad0372311ee3ca437
blaisewf <blaise@applio.org>
2025-06-12 11:25:27 +0200

    fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,9 +85,8 @@ export default function RootLayout({
 	return (
 		<html lang="en" className="dark">
 			<head>
-				<link rel="icon" type="image/svg+xml" href="/favicon.ico" />
-				<link rel="shortcut icon" href="/favicon.ico" />
-				<link rel="apple-touch-icon" href="/favicon.ico" />			</head>
+				<link rel="icon" type="image/svg+xml" href="/favicon.ico" /><link rel="shortcut icon" href="/favicon.ico" /><link rel="apple-touch-icon" href="/favicon.ico" />
+			</head>
 			<body className={`${poppins.className} font-sans bg-[#080808]`}>
 				{/* google tag manager */}
 				<Script id="gtm-script" strategy="afterInteractive">
