93ae0444cd2ce3f60b994056f2d843cced46ffb2
deeivihh <deeivihh@hotmail.com>
2025-06-15 22:37:59 +0200

    Use cloudflare functions

--- src/app/api/beta/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { handleWebhook } from './webhook-handler';
-
-export const runtime = 'edge';
-
-export async function POST(request: Request) {
-	return handleWebhook(request);
-}
