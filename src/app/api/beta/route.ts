871a42354e5f30f61bedcc447e3540a784def68f
deeivihh <deeivihh@hotmail.com>
2025-06-15 22:26:16 +0200

    Fix

--- src/app/api/beta/route.ts
+++ src/app/api/beta/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 
-export const config = {
-    runtime: 'edge',
-}
+export const runtime = 'edge';
 
 // handle webhook submission securely
 export async function POST(request: Request) {
