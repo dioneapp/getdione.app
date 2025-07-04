f97f0217c976f897d74d561ee26941d62661c64a
deeivihh <deeivihh@hotmail.com>
2025-07-04 16:14:03 +0200

    Accept avatar images on `next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,45 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "cdn.discordapp.com",
+				pathname: "/avatars/**",
+			},
+			{
+				protocol: "https",
+				hostname: "cdn.discordapp.com",
+				pathname: "/embed/avatars/**",
+			},
+			{
+				protocol: "https",
+				hostname: "avatars.githubusercontent.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "lh3.googleusercontent.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "i.imgur.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "github.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "pbs.twimg.com",
+				pathname: "/**",
+			},
+		],
+  },
 };
 
 export default nextConfig;
