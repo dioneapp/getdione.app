c9df8d2d269199bac36efb54aa2f5bc22f49c245
blaisewf <blaise@applio.org>
2025-07-04 23:24:40 +0200

    fix build by adding types

--- src/components/explore/ExploreGrid.tsx
+++ src/components/explore/ExploreGrid.tsx
@@ -1,6 +1,23 @@
 import ExploreCard from "./ExploreCard";
 
-export default function ExploreGrid({ scripts }) {
+interface CardProps {
+	id: number;
+	name: string;
+	description: string;
+	author: string;
+	author_url: string;
+	logo_url: string;
+	banner_url: string;
+	script_url: string;
+	version: string;
+	tags: string[];
+	likes: number;
+	downloads: number;
+	created_at: string;
+	updated_at: string;
+}
+
+export default function ExploreGrid({ scripts }: { scripts: CardProps[] }) {
 	return (
 		<section
 			className="mt-8 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 max-w-7xl"
