066a02d4870ce4e0f0ac4fad5f1dc6dda8e7cdd4
deeivihh <deeivihh@hotmail.com>
2025-07-19 06:53:58 +0200

    Undo commit

--- apps/web/src/app/auth/callback/[...params]/route.ts
+++ apps/web/src/app/auth/callback/[...params]/route.ts
@@ -3,9 +3,9 @@ import createClient from "@/utils/supabase/server";
 
 export async function GET(request: Request, context: any) {
 	const { searchParams, origin } = new URL(request.url);
-	const { params } = context;
+	const real = await context.params;
 
-	const isApp = Array.isArray(params) ? params.includes("app") : false;
+	const isApp = real.params.includes("app");
 	const code = searchParams.get("code");
 
 	if (code) {
